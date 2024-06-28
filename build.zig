bc2defabed5b8cbc5a8629017d619cd6f5e7d902
superp00t <superp00t@tutanota.com>
2024-06-28 00:03:38 -0400

    fix(build): fix build.zig

--- build.zig
+++ build.zig
@@ -1,11 +1,14 @@
 const std = @import("std");
+const system = @import("zig/system.zig");
 
-pub fn build(b: *std.Build) void {
-  const optimize = b.standardOptimizeOption(.{});
-  const module = b.addModule(.{
-    .name = "system",
-    .root_source_file = b.path("zig/system.zig"),
-    .optimize = optimize
-  });
-  b.default_step.dependOn(&module.step);
-}
+// pub fn build(b: *std.Build) void {
+//   // const optimize = b.standardOptimizeOption(.{});
+//   // const module = b.addModule(.{
+//   //   .name = "system",
+//   //   .root_source_file = b.path("zig/system.zig"),
+//   //   .optimize = optimize
+//   // });
+//   // b.default_step.dependOn(&module.step);
+// }
+
+pub const add_defines = system.add_defines;
